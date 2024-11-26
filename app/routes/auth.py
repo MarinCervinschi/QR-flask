c0b7b1a90a453f7ca65796c51d6392bd2ed4fd23
MarinCervinschi <marincervinschi3@gmail.com>
2024-11-26 19:11:31 +0100

    feat: dashboard route

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -7,6 +7,7 @@ from werkzeug.security import check_password_hash
 
 bp = Blueprint('auth', __name__, url_prefix='/private')
 
+
 def json_data(description, data):
     columns = [column[0] for column in description]
     return [dict(zip(columns, row)) for row in data]
@@ -46,36 +47,11 @@ def admin():
         if error is None:
             session.clear()
             session['user'] = user
-            return redirect(url_for('auth.dashboard'))
+            return redirect(url_for('auth.dashboard.dashboard'))
 
         flash(error)
     
     return render_template('admin.html')
 
-
-def get_links():
-    try:
-        db = get_db()
-        cur = db.cursor()
-        cur.execute("SELECT * FROM dynamic_links")
-        description = cur.description
-        links = cur.fetchall()
-    except Exception as e:
-        flash(f"An error occurred: {e}")
-        links = None
-    finally:
-        cur.close()
-
-    if links is not None:
-        links = json_data(description, links)
-
-    print(links)
-    return links
-
-@bp.route('/admin/dashboard')
-def dashboard():
-    if 'user' in session:
-        return render_template('dashboard.html', links=get_links())
-    else:
-        return redirect(url_for('auth.admin'))
-    
+from . import dashboard
+bp.register_blueprint(dashboard.bp)
---
+++ app/routes/dashboard.py
@@ -0,0 +1,37 @@
+from flask import (
+    Blueprint, flash, render_template, request, redirect, url_for, session)
+
+from ..db import get_db
+
+bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
+
+def json_data(description, data):
+    columns = [column[0] for column in description]
+    return [dict(zip(columns, row)) for row in data]
+
+def get_links():
+    try:
+        db = get_db()
+        cur = db.cursor()
+        cur.execute("SELECT * FROM dynamic_links")
+        description = cur.description
+        links = cur.fetchall()
+    except Exception as e:
+        flash(f"An error occurred: {e}")
+        links = None
+    finally:
+        cur.close()
+
+    if links is not None:
+        links = json_data(description, links)
+
+    print(links)
+    return links
+
+@bp.route('/')
+def dashboard():
+    if 'user' in session:
+        return render_template('dashboard.html', links=get_links())
+    else:
+        return render_template('status.html', status="401")
+    
