846a1b97e499967afcb2bae0a25dd9018db91fa0
MarinCervinschi <marincervinschi3@gmail.com>
2024-11-30 11:56:33 +0100

    fix: data.sql

--- tests/data.sql
+++ tests/data.sql
@@ -1,6 +1,12 @@
+CREATE DATABASE IF NOT EXISTS test_db;
+
+USE test_db;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
 -- Insert test users
 INSERT INTO users (username, password) VALUES ('test', 'scrypt:32768:8:1$zDTU2CWuivDwFAfb$a8e74f1bdaf2bccdf3b5021aadddfa2f7e38210d418167330e90af07914c55d820245975e991b91dc9db66b99110bece037f524d139921608e4276f5f6be5e10');
-
--- Insert test links
-INSERT INTO dynamic_links (internal, external) VALUES ('test_link1', 'http://example.com/1');
-INSERT INTO dynamic_links (internal, external) VALUES ('test_link2', 'http://example.com/2');
