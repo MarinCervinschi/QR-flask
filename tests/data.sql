80b8b679061e68400ac67446c3d509abf4557e99
MarinCervinschi <marincervinschi3@gmail.com>
2024-11-30 11:59:32 +0100

    fix: data.sql

--- tests/data.sql
+++ tests/data.sql
@@ -8,5 +8,7 @@ CREATE TABLE IF NOT EXISTS users (
   password VARCHAR(255) NOT NULL
 );
 
+DELETE FROM users;
+
 -- Insert test users
 INSERT INTO users (username, password) VALUES ('test', 'scrypt:32768:8:1$zDTU2CWuivDwFAfb$a8e74f1bdaf2bccdf3b5021aadddfa2f7e38210d418167330e90af07914c55d820245975e991b91dc9db66b99110bece037f524d139921608e4276f5f6be5e10');
